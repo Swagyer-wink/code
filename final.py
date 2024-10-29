afb507de66ff5fa64953d225a6bbb030492a659f
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-10-29 19:44:07 +0000

    comunisim

--- final.py
+++ final.py
@@ -1,17 +1,23 @@
-# make player
 
-# name player
+#make player - Jayda (variable)
+#the players walks down a path and comes across a fork in the road- sawyer
 
-# have player get stranded
+#the player is on the road and has to make a choice of left or right- sawyer(Function)
 
-# meet someone
+#if right they go to hell where they have to fight there way out- sawyer
 
-# have the person give the player a task
+#if they choose left they go heaven where they have tea with god then get smited back to earth - sawyerr
 
-#have the player go on the task
+#the player runs into to two people ad have to chose which to trust in hell-Kathryn
 
-#have the player complete the task
+#if they trust the first hell person hey gdrown in the near by river-Kathrn
 
-#have the player go back to the person
+#if they trust the second hell person they get out of hell and into earth-Kathryn
 
-#have the player get a reward or go home
+#if the payer choses the first task they have to beat a bear - Sawyer-(loops)
+
+ #the second task is to get sacrficed for the local cult - Jayaydada (conditionals)
+
+#if they complete the first they become the new king or they bec 
+ 
+ 
