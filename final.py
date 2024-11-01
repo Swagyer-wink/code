2e90348bd7e2c4c7bf88ba5b8721f34c59ec6e7f
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-11-01 19:41:48 +0000

    help me

--- final.py
+++ final.py
@@ -1,19 +1,40 @@
-#When game starts a random word is generated and a hang man board is put up- all-Kathryn(CONDITIONAL)
-
-#then a hangman board is printed on the screen-kathryn(VARIABLE)
-
+#When game starts a random word is generated-Kathryn(function)
+print("Welcome to Brainrot Hangman!")
+import random 
+ #then a hangman board is printed on the screen-kahtryn(VARIABLE)
+print("  ___________")
+print("  |")
+print("  |")
+print("  |")
+print("  |")
+print("  |")
+print("_____________")
 #the slots for the word will apper-kathryn(CONDITIONAL)
-
+word = ['Nails','Sigma','Bussin','Skibdi','Big Back' , 'Rizz' , 'Fanthm Tax'] 
+wordChoice = random.choice(word)
+for x in word:
+  if x == random:
+     print(x)
 #the player has to guess a letter-sawyer(LOOP)
+for words in word:
+    print(input("guess a letter:"))
 
-#if its correct the letter will print and they will play again-sawyer(LOOP)
-
-#if its wrong then its a x will print on the screen and they have to guess again-sawyer(LOOP)(a function will be added)
+#if its correct the letter will print and they will play again-sawyer(CONDITONAL)
+if 
+#if its wrong then its a x will print on the screen and they have to guess again-sawyer(CONDITONAL))(a function will be added)
 
 #if player 1 gets it wrong six times they die - jayda(CONDITIONALS)
-
+guess = 6
+if guess <= 6:
+    print("you guess 6 words 6 times\n")
 #if the player gets it all right they win - jayda(VARIABLE)(Add a function)
+win = input("you have won")
+print("you won the game\n")
+#a play agian option is printed on the screen- jayda(VARIABLE)
+play = ("you can play again")
+print("go play again\n")
 
-# a play agian option is printed on the screen- jayda(VARIABLE)
  
+
+
 
