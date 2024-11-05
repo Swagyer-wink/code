cdb32533341e618349c56cc3a29cd56f9ae5f988
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-11-05 20:44:06 +0000

    help

--- final.py
+++ final.py
@@ -11,25 +11,27 @@ print("  |")
 print("_____________")
 #the slots for the word will apper-kathryn(CONDITIONAL)
 word = ['nails','sigma','bussin','skibidi','big back' , 'rizz' , 'fanum tax'] 
-letter = ["i", "d", "n", "a", "l", "s", "b", "c", "k", "f", "t", "m", "z", "u"]
+
 wordChoice = random.choice(word)
 for x in word:
   if x == random:
      print(x)
 #the player has to guess a letter-sawyer(LOOP)
+letter = ["i", "d", "n", "a", "l", "s", "b", "c", "k", "f", "t", "m", "z", "u"]
+notletter = ["e", "h", "j", "o", "p", "y", "w", "q",]
+theGuess = [print(input("guess a letter:"))]
 for letter in word:
       print(input("guess a letter:"))
-      if letter in word:
+theGuess = [print(input("guess a letter:"))]
+if theGuess in word:
           print("this is right")
-      else:
-          print("this is incorrect try again")
-notletter = ["e", "h", "j", "o", "p", "y", "w", "q",]
+elif notletter in word:
+        print("this is incorrect try again")
+
 #if its correct the letter will print and they will play again-sawyer(CONDITONAL)
-for letter in wordChoice:
-   print("it is in the word")
+
 #if its wrong then its a x will print on the screen and they have to guess again-sawyer(CONDITONAL))(a function will be added)
-else:
-   print("THIS IS INCORRECT")
+
 #if its wrong then its a x will print on the screen and they have to guess again-sawyer(CONDITONAL))(a function will be added)
 #if player 1 gets it wrong six times they die - jayda(CONDITIONALS)
 guess = 6
@@ -38,6 +40,7 @@ if guess <= 6:
 #if the player gets it all right they win - jayda(VARIABLE)(Add a function)
 win = input("you have won")
 print("you won the game\n")
+
 #a play agian option is printed on the screen- jayda(VARIABLE)
 play = ("you can play again")
 print("go play again\n")
