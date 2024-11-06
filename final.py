f2fced7f22dff1914dd2a7a9e7520df03c7a9fdd
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-11-06 21:28:40 +0000

    cooking

--- final.py
+++ final.py
@@ -14,42 +14,35 @@ print("_____________")
 wordchoice = ['nails','sigma','bussin','skibidi','big back' , 'rizz' , 'fanum tax'] 
 def game(wordchoice):
    pass
-wordChoice = random.choice(word)
+wordChoice = random.choice(wordchoice)
 word = game
 
 #the player has to guess a letter-sawyer(LOOP)
-correctLetter = ["i", "d", "n", "a", "l", "s", "b", "c", "k", "f", "t", "m", "z", "u"]
-notletter = ["e", "h", "j", "o", "p", "y", "w", "q",]
+correctLetter = ["i", "d", "r", "n", "a", "l", "s", "b", "c", "k", "f", "t", "m", "z", "u"]
+notLetter = ["e", "h", "j", "o", "p", "y", "w", "q"]
 guess = 6
 for correctLetter in wordchoice:
-  theGuess = input("guess a letter:")
+   theGuess = input("guess a letter:")
 
 #if its correct the letter will print and they will play again-sawyer(CONDITONAL)
-if correctLetter in wordchoice:
-   print("this is right")
+   if theGuess in correctLetter:
+      print("this is right")
 #if its wrong then its a x will print on the screen and they have to guess again-sawyer(CONDITONAL))(a function will be added)
-else:
-    print("x\n", "this is incorrect try again")
-#if its wrong then its a x will print on the screen and they have to guess again-sawyer(CONDITONAL))(a function will be added)
-if correctLetter in wordchoice:
-   print("this is right")
-else:
-    print("x\n", "this is incorrect try again")
+   else:
+      print("x\n", "this is incorrect try again")
 #if player 1 gets it wrong six times they die - jayda(CONDITIONALS)
-if guess<= 6:
+if guess<= 5:
     print("you have used up all your guess\n")
 #if the player gets it all right they win - jayda(VARIABLE)(Add a function)
- 
-if word:
+wordGuess = input("final guess is to guess a word:")
+if wordGuess is wordchoice:
    print("you have won the game")
+else:
+   print("you got it wrong")
 
 #a play agian option is printed on the screen- jayda(VARIABLE)
-inp = [input("do you want to play again:")]
+inp = input("do you want to play again:")
 if inp == "yes":
-   print("click run again")
-elif inp == "no":
-   print("get out of here")
- 
-
-
-
+   print("please rerun")
+else:
+   print("please leave")
