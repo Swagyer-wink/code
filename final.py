ebe0b48054f10575bafedc6069e806678f1e846c
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-11-07 00:54:05 -0700

    Update final.py

--- final.py
+++ final.py
@@ -2,30 +2,66 @@
 
 print("Welcome to Brainrot Hangman!")
 import random 
- #then a hangman board is printed on the screen-kahtryn(VARIABLE)
-print("  ___________")
+wordcheck =""
+win=0
+print("  ___________")  
 print("  |")
 print("  |")
 print("  |")
 print("  |")
 print("  |")
 print("_____________")
+
+def game():
+    global things_left
+
+    global guessuedLetters
+    global choseWord
+    print("a new word was chose")
+    #then a hangman board is printed on the screen-kahtryn(VARIABLE)
+    print("  ___________")  
+    print("  |")
+    print("  |")
+    print("  |")
+    print("  |")
+    print("  |")
+    print("_____________")
 #the slots for the word will apper-kathryn(CONDITIONAL)
-wordchoice = ['nails','sigma','bussin','skibidi','big back' , 'rizz' , 'fanum tax'] 
-def game(wordchoice):
-   pass
-wordChoice = random.choice(wordchoice)
+things_left = 6
+words = ["fanum tax", "sigma", "bussin", "skibidi", "big back", "rizz"] 
 word = game
+guessuedLetters = []
+choseWord = random.choice(words)
+wordcheck = ""
+def game():
+    global things_left_left
+
+    global guessuedLetters
+    global choseWord
+    print("a new word was chose")
+    
+    guessuedLetters = []
+    choseWord = random.choice(words)
+    wordcheck = ""
+    things_left = 6
+    game()
+def restart():
+    answer = input("Restart (y/n):")
+    if answer == "y":
+        restart()
+    elif answer == "n":
+        exit()
+    restart()
 
 #the player has to guess a letter-sawyer(LOOP)
 correctLetter = ["i", "d", "r", "n", "a", "l", "s", "b", "c", "k", "f", "t", "m", "z", "u"]
 notLetter = ["e", "h", "j", "o", "p", "y", "w", "q"]
 guess = 6
-for correctLetter in wordchoice:
+for correctLetter in words:
    theGuess = input("guess a letter:")
 
 #if its correct the letter will print and they will play again-sawyer(CONDITONAL)
-   if theGuess in correctLetter:
+   if theGuess in choseWord:
       print("this is right")
 #if its wrong then its a x will print on the screen and they have to guess again-sawyer(CONDITONAL))(a function will be added)
    else:
@@ -35,14 +71,13 @@ if guess<= 5:
     print("you have used up all your guess\n")
 #if the player gets it all right they win - jayda(VARIABLE)(Add a function)
 wordGuess = input("final guess is to guess a word:")
-if wordGuess is wordchoice:
+if wordGuess == choseWord:
    print("you have won the game")
 else:
    print("you got it wrong")
-
+answer = input("Restart (y/n):")
 #a play agian option is printed on the screen- jayda(VARIABLE)
-inp = input("do you want to play again:")
-if inp == "yes":
-   print("please rerun")
+if answer == "y":
+   print("rerun to replay")
 else:
    print("please leave")
